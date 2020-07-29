c102a0bfc203854479985721e11a983d03178c61
kuarisu01 <54207095+kuarisu01@users.noreply.github.com>
2020-07-29 23:00:11 +0900

    add 現状の成果物
    リニューアル前のサイトをコミット

---
+++ パンチ☆ぱんてぃ/js/main.js
@@ -0,0 +1,14 @@
+(function() {
+    //エラーチェック用
+    'use strict'
+
+    
+    // two way data binding (to UI)
+
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            name: 'Captain'
+        }
+    });
+})();
