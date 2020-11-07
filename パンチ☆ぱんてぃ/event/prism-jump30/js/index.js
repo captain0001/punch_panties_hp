61b66cd16f9e522063128c07ae6252bf01fd07d2
captain0001 <54207095+kuarisu01@users.noreply.github.com>
2020-11-07 19:08:12 +0900

    fix ダウンロードボタンが表示される修正

--- パンチ☆ぱんてぃ/event/prism-jump30/js/index.js
+++ パンチ☆ぱんてぃ/event/prism-jump30/js/index.js
@@ -5,18 +5,21 @@ $(function() {
     //   console.log(json); // this will show the info it in firebug console
     // });
     const $download_01 = $('.download_01');
-    let key = '';
+    let count = 0;
     function download01() {
       console.log('ダウンロードされました');
       location.href = './data/01/くありす本物語_01.jpg';
     }
     function displayButton() {
-      
+      $('.form').append('<a href="./data/01/くありす本物語_01.jpg" class="button download" download>ダウンロード</a>');
     }
     function match01() {
       console.log('押下');
       const json = $.getJSON("./js/json/pass.json");
-      displayButton(pass);
+      if (count < 1 ){
+        displayButton();
+      }
+      count++ ;
     }
     function events() {
       $download_01.on('click', match01);
