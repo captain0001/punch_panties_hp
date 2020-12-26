6d8b6145850c4f32113f57977787bc754b334c71
captain0001 <54207095+kuarisu01@users.noreply.github.com>
2020-12-26 13:05:41 +0900

    fix レイアウト修正

---
+++ パンチ☆ぱんてぃ/event/prism-jump31/js/index.js
@@ -0,0 +1,43 @@
+
+$(function() {
+  function downloadMatch() {
+    const $download_01 = $('.download_01');
+    let count = 0;
+    let successFlg = false;
+    function displayButton() {
+      $('.form').append('<a href="https://bit.ly/3hjLRLx" class="button download" download>ダウンロード</a>');
+    }
+    function match01() {
+      console.log('押下');
+      const nameEnter = $('.download-001_name').val();
+      const passEnter = $('.download-001_pass').val();
+      console.log(nameEnter,passEnter);
+      if (count < 1 ){
+        $.getJSON("https://script.google.com/macros/s/AKfycby1FT-gVg42g99jVW65C65kb7fRWq4FiLKatYob6U2YAfB3fXWh/exec", function(json) {
+          json.forEach(value => {
+            if(value.password === passEnter && value.name === nameEnter){
+              console.log('一致しましたね');
+              displayButton();
+              successFlg = true;
+              count++ ;
+            }
+          });
+          if(successFlg === false){
+            $('.error').remove();
+            $('.make01').append('<p class="error">※idとpassのどちらかが間違えてます</p>');
+          } else {
+            $('.error').remove();
+          }
+        });
+      }
+    }
+    function events() {
+      $download_01.on('click', match01);
+    }
+    function init() {
+      events();
+    }
+    init();
+  }
+  downloadMatch();
+});
